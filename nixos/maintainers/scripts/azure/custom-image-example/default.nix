ed0dcb11c4357696421fbf6e640a1750b7152e8d
Cole Mickens <cole.mickens@gmail.com>
2019-06-04 03:14:39 +0000

    nixos/azure: update custom-image-example

--- nixos/maintainers/scripts/azure/custom-image-example/default.nix
+++ nixos/maintainers/scripts/azure/custom-image-example/default.nix
@@ -1,10 +1,13 @@
-{ lib, pkgs, ... }:
-
-with lib;
-{
-  imports = [ ./azure-common.nix ];
-
-  services.openssh.permitRootLogin = "no";
-  security.sudo.wheelNeedsPassword = false;
-  networking.usePredictableInterfaceNames = lib.mkForce true;
+let
+  rev = "2eaad3ec3e43a0cfa49ca6524c518c86eb7c34b5";
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/colemickens/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = "06c10c5ayxjhim4c5a3xf3f71g3alklf7wh8lnp1vr5qr6kw4ccc";
+  };
+  mkAzureImage = (import "${nixpkgs}/nixos/modules/virtualisation/azure-mkimage.nix").mkAzureImage;
+in mkAzureImage {
+  inherit nixpkgs;
+  inherit rev;
+  configFile = ./config.nix;
+  diskSize = 2048;
 }
