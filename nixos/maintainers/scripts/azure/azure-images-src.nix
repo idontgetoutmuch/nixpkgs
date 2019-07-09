5c15e10072811b4dfd21693595b1e45b66681c6a
Cole Mickens <cole.mickens@gmail.com>
2019-07-09 21:57:04 +0000

    azure: bump up image

--- nixos/maintainers/scripts/azure/azure-images-src.nix
+++ nixos/maintainers/scripts/azure/azure-images-src.nix
@@ -1,11 +1,11 @@
 let
   mkAzureImage = (import ../../../modules/virtualisation/azure-mkimage.nix).mkAzureImage;
 in rec {
-  nixos_19_03__606306e0eaacdaba1d3ada33485ae4cb413faeb5 = mkAzureImage rec {
-    rev = "606306e0eaacdaba1d3ada33485ae4cb413faeb5";
+  nixos_19_03__9ec7625cee5365c741dee7b45a19aff5d5d56205 = mkAzureImage rec {
+    rev = "9ec7625cee5365c741dee7b45a19aff5d5d56205";
     nixpkgs = builtins.fetchTarball {
       url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
-      sha256 = "06c10c5ayxjhim4c5a3xf3f71g3alklf7wh8lnp1vr5qr6kw4ci7";
+      sha256 = "0rh26fhdvnp9ssk8g63ysyzigw9zg43k9bd2fzrvhrk75sav723h";
     };
   };
 }
