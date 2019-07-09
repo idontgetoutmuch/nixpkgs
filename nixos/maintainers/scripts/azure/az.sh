d120c74d62d54e7f59356a3b25a29d09c924b3d1
Cole Mickens <cole.mickens@gmail.com>
2019-07-09 22:14:33 +0000

    more small fixes

--- nixos/maintainers/scripts/azure/az.sh
+++ nixos/maintainers/scripts/azure/az.sh
@@ -5,6 +5,7 @@ sudo docker run \
   -v /nix:/nix \
   -v /tmp/azure-cli:/tmp/azure-cli \
   -e "AZURE_CONFIG_DIR=/tmp/azure-cli" \
+  -e "AZURE_USER=${AZURE_USER:-"azureuser"}" \
   -e "AZURE_STORAGE_CONNECTION_STRING=${AZURE_STORAGE_CONNECTION_STRING}" \
   microsoft/azure-cli:latest az "${@}"
 
--- nixos/maintainers/scripts/azure/mkvm.sh
+++ nixos/maintainers/scripts/azure/mkvm.sh
@@ -9,10 +9,10 @@ function cleanup() {
   fi
 }
 
-export AZURE_HOSTNAME="xx${AZURE_HOSTNAME:-"nixos-${RANDOM}"}"
-export AZURE_LOCATION="${AZURE_LOCATION:="westus2"}"
+export AZURE_HOSTNAME="${AZURE_HOSTNAME:-"nixos-${RANDOM}"}"
+export AZURE_LOCATION="${AZURE_LOCATION:="westeurope"}"
 export AZURE_SIZE="${AZURE_SIZE:-"Standard_D4s_v3"}"
-export AZURE_RESOURCE_GROUP="${AZURE_HOSTNAME}-${AZURE_LOCATION}"
+export AZURE_RESOURCE_GROUP="${AZURE_RESOURCE_GROUP:-"${AZURE_HOSTNAME}-${AZURE_LOCATION}"}"
 export AZURE_SUBSCRIPTION_ID="${AZURE_SUBSCRIPTION_ID:-"aff271ee-e9be-4441-b9bb-42f5af4cbaeb"}" 
 
 sshpubkey="$(ssh-add -L)"
@@ -25,8 +25,10 @@ az group create \
 SUCCESS="false"
 trap cleanup EXIT
 
-export AZURE_USER="${USER}"
+export AZURE_USER="${AZURE_USER:-"${USER:-}"}"
 [[ "${AZURE_USER}" == "root" ]] && export AZURE_USER="azureuser"
+[[ "${AZURE_USER}" == "" ]] && export AZURE_USER="azureuser"
+
 export AZURE_PASSWORD="${AZURE_PASSWORD:-"TheN!x0sL0r!s"}"
 
 az vm create \
